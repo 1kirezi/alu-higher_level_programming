85d5aac0c1e7991c27f298ca373e2d9f27f3d8b2
Divine Ikirezi <d.ikirezi2@alustudent.com>
2025-03-06 16:41:32 -0800

    Python exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,4 +4,14 @@ def safe_print_integer(value):
         print("{:d}".format(value))
     except:
         return False
-    return True
+    return True#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num += 1
+        except(TypeError, ValueError):
+            continue
+    print()
+    return num
